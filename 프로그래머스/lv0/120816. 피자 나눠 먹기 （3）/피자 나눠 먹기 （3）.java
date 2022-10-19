af7d3cd2a5e01c72dad6dbb2d8330bffc3f723a2
yunicho <47688544+yunicho@users.noreply.github.com>
2022-10-19 14:39:09 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int pizza = 0;
+        
+        for(pizza = 0; slice*pizza < n; ++pizza) {
+        }
+        return pizza;
+    }
+}
